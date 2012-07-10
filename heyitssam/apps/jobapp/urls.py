93c3083e23947fc6f0d57f118ef9d3d57a9ca33f
ericlindstrom <eric.lindstrom@gmail.com>
2012-07-10 08:22:01 -0700

    updates to jobapp

--- heyitssam/apps/jobapp/urls.py
+++ heyitssam/apps/jobapp/urls.py
@@ -1,8 +1,11 @@
 from django.conf.urls.defaults import patterns, include, url
 from django.conf import settings
+from models import *
 from views import *
+from django.views.generic import TemplateView, DetailView
 
 urlpatterns = patterns('',
+    url(r'^(?P<slug>.*)/contact/$', contact_view, name='contact'),
     url(r'^(?P<slug>.*)/resume/$', resume_view, name='resume'),
     url(r'^(?P<slug>.*)/cv/$', cv_view, name='cv'),
     url(r'^(?P<slug>.*)/$', overview, name='overview'),
--- heyitssam/apps/jobapp/views.py
+++ heyitssam/apps/jobapp/views.py
@@ -5,6 +5,10 @@ from django.views.generic import DetailView, TemplateView
 class CompanyDetailView(DetailView):
     model = JobApp
 
+class ContactView(DetailView):
+    model=JobApp
+    template_name="jobapp/contact.html"
+
 class CVDetailView(DetailView):
     model = JobApp
     template_name = 'jobapp/cv.html'
@@ -16,3 +20,4 @@ class ResumeDetailView(DetailView):
 overview = CompanyDetailView.as_view()
 cv_view = CVDetailView.as_view()
 resume_view = ResumeDetailView.as_view()
+contact_view = ContactView.as_view()
