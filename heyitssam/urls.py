1cec2cea53c6ef2700cb6f7632c7d76a1b407573
ericlindstrom <eric.lindstrom@gmail.com>
2012-07-10 21:24:33 -0700

    robots

--- heyitssam/urls.py
+++ heyitssam/urls.py
@@ -1,11 +1,13 @@
 from django.conf.urls.defaults import patterns, include, url
 from django.contrib import admin
 from django.conf import settings
+from apps.jobapp.models import *
 admin.autodiscover()
 
 urlpatterns = patterns('',
     url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^robots.txt$', 'django.views.generic.simple.direct_to_template', {'template':'robots.txt', 'mimetype':'text/plain'},),
     url(r'^', include('apps.jobapp.urls', namespace='jobapp')),
     #url(r'^heyitssam/', include('heyitssam.foo.urls')),
 )
