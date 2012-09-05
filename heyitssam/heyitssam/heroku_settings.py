8f5fdda50b101619a3ed0f753ea5fb54ee01dae3
ericlindstrom <eric.lindstrom@gmail.com>
2012-09-05 00:12:04 -0700

    install boto

--- heyitssam/heyitssam/heroku_settings.py
+++ heyitssam/heyitssam/heroku_settings.py
@@ -10,6 +10,7 @@ AWS_ACCESS_KEY_ID = os.environ['S3_KEY']
 AWS_SECRET_ACCESS_KEY = os.environ['S3_SECRET'] 
 AWS_STORAGE_BUCKET_NAME = 'public.heyitssam.com'
 
-STATIC_URL = '//public.heyitssam.com/static/'
-MEDIA_URL = '//public.heyitssam.com/media/'
+STATIC_URL = '//%s/static/' % AWS_STORAGE_BUCKET_NAME
+MEDIA_URL = '//%s/media/' % AWS_STORAGE_BUCKET_NAME
+ADMIN_MEDIA_PREFIX = '//%s/admin/' % AWS_STORAGE_BUCKET_NAME
 
