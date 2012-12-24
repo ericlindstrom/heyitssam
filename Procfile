2c1ffb8f0faef87b3298e40884a06b93aaa79103
ericlindstrom <eric.lindstrom@gmail.com>
2012-12-24 14:03:34 -0800

    static

--- Procfile
+++
@@ -1,1 +0,0 @@
-web: newrelic-admin run-program python heyitssam/manage.py runserver 0.0.0.0:$PORT --settings=heyitssam.heroku_settings
--- heyitssam/heyitssam/apps/jobapp/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from models import *
-
-class JobAppAdmin(admin.ModelAdmin):
-    prepopulated_fields = {'slug':('company_name',)}
-    list_display = ('position', 'company_name', 'visible',)
-    list_editable = ('visible',)
-
-admin.site.register(JobApp, JobAppAdmin)
